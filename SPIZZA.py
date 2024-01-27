4f4012fd0e4cbc4b349032aa800b74c47029e0db
Bruno Moreira <moreirabcf04@gmail.com>
2024-01-27 18:34:29 -0300

    Update SPIZZA.py

--- SPIZZA.py
+++ SPIZZA.py
@@ -9,7 +9,16 @@ class Sabores():
             '14 - Banana com Canela', '15 - Brocolis com Catupiry', '16 - Margherita'
         ]
 
-        self.tipos_bordas = ['1 - Tradicional', '2 - Catupiry', '3 - Cheddar', '4 - Chocolate', '5 - Recheada com Presunto e Queijo', '6 - Sem Borda']
+        self.tipos_bordas = [
+            '\033[92m1 - Voltar\033[0m', '2 - Catupiry', '3 - Cheddar', '4 - Chocolate', '         5 - Recheada com Presunto e Queijo', '6 - Sem Borda',
+                               '7 - Tradicional'
+                               ]
+
+        self.tipos_refri = [
+            '\033[92m1 - Voltar\033[0m', '2 - Coca-Cola', '3 - Pepsi', '4 - Guaraná Antarctica', '5 - Fanta',
+              '         6 - Sprite', '7 - Schweppes', '8 - Kuat', '9 - Sukita', '10 - Dolly Guaraná'
+              ]
+
 
 
     def um_sabor(self):
@@ -29,12 +38,11 @@ class Sabores():
         if sabor_selecionado in range(1, len(self.tipos_sabores) + 1):
             sabor_escolhido = self.tipos_sabores[sabor_selecionado - 1]
             if sabor_selecionado == 1:
-                main_instance.limpeza()
-                main_instance.cardapio()
+                self.voltar()
             else:
                 print(f'\nVocê selecionou: \033[92m{sabor_escolhido.split(" - ")[1]}\033[0m\n')
                 input('Pressione Enter para continuar...')
-                self.bordas()
+                self.borda()
         else:
             print('-------------------------')
             print('\033[91m\nSelecione um valor válido.\033[0m\n')
@@ -59,8 +67,7 @@ class Sabores():
         else:            
             sabor_selecionado2 = int(input('\nQual o segundo sabor da sua Pizza? '))
             if sabor_selecionado2 == 1:
-                main_instance.limpeza()
-                main_instance.cardapio()
+                self.voltar()
             else:
                 if sabor_selecionado1 in range(1, len(self.tipos_sabores) + 1) and sabor_selecionado2 in range(1, len(self.tipos_sabores) + 1):
                     sabor_escolhido1 = self.tipos_sabores[sabor_selecionado1 - 1]
@@ -72,18 +79,64 @@ class Sabores():
 
         print(f'\nVocê selecionou: \033[92m{sabor_escolhido1.split(" - ")[1]}\033[0m e \033[92m{sabor_escolhido2.split(" - ")[1]}\033[0m\n')
         input('Pressione Enter para continuar...')
-        self.bordas()
+        self.borda()
 
+
+    def borda(self):
+        main_instance.limpeza()
+
+        primeira_parte = self.tipos_bordas[:4]
+        segunda_parte = self.tipos_bordas[4:]
+
+        print('Possuímos as seguintes bordas:\n')
+        for borda1, borda2 in zip(primeira_parte, segunda_parte):
+            print(f'{borda1:<40} {borda2}\n')
+        print('-------------------------')
+        borda_selecionada = int(input('Qual tipo de borda você deseja? '))
+        if borda_selecionada == 1:
+            self.voltar()
+            
+        elif borda_selecionada in range(1, len(self.tipos_bordas) + 1):
+            borda_escolhida = self.tipos_bordas[borda_selecionada - 1]
+            
+        else:
+            print('-------------------------')
+            print('\033[91m\nSelecione um valor válido.\033[0m\n')
+            self.borda()
+
+        print(f'\nVocê selecionou: \033[92m{borda_escolhida.split(" - ")[1]}\033[0m\n')
+        input('Pressione Enter para continuar...')
+        self.refri()
+    
+
+    def refri(self):
+        main_instance.limpeza()
+
+        primeira_parte = self.tipos_refri[:5]
+        segunda_parte = self.tipos_refri[5:]
+
+        print('Possuímos os seguintes tipos de refri:\n')
+        for refri1, refri2 in zip(primeira_parte, segunda_parte):
+            print(f'{refri1:<40} {refri2}\n')
+        print('-------------------------')
+        refri_selecionado = int(input('Que tipo de refri você deseja? '))
+        if refri_selecionado == 1:
+            self.voltar()
+        elif refri_selecionado in range(1, len(self.tipos_refri) + 1):
+            refri_escolhido = self.tipos_refri[refri_selecionado - 1]
+        else:
+            print('-------------------------')
+            print('\033[91m\nSelecione um valor válido.\033[0m\n')
+            self.refri()
+
+        print(f'\nVocê selecionou: \033[92m{refri_escolhido.split(" - ")[1]}\033[0m\n')
+        input('Pressione Enter para continuar...')
+        endereco_instance.endereco()
 
     def voltar(self):
         main_instance.limpeza()
         main_instance.cardapio()
 
-
-    def bordas(self):
-        main_instance.limpeza()
-
-        
 
 class Main():
     def limpeza(self):
@@ -140,6 +193,24 @@ class Main():
             sabores_instance.dois_sabores()
 
 
+class Endereco():
+    def __init__(self):
+        self.rua = []
+        self.bairro = []
+        self.cep = []
+
+
+    def endereco(self):
+        main_instance.limpeza()
+        msg = 'Preencha o endereço de entrega do pedido:'
+        quantidade_letras = 0
+        for letra in msg:
+            quantidade_letras += 1
+        print(msg)
+        print('-'*quantidade_letras)
+        print('\nRua: ')
+
+
 if __name__ == "__main__":
     main_instance = Main()
     main_instance.limpeza()
@@ -150,4 +221,7 @@ if __name__ == "__main__":
     sabores_instance.um_sabor()
     sabores_instance.dois_sabores()
     sabores_instance.voltar()
-    sabores_instance.bordas()
+    sabores_instance.borda()
+    sabores_instance.refri
+    endereco_instance = Endereco()
+    endereco_instance.endereco()
